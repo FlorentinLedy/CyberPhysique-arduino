aca08aea062c2aadf2d26f3614232b01b81e3cd4
Florentin <klach1703@gmail.com>
2024-06-19 09:50:07 +0200

    Ajout vitesse angle

--- RobotUltimateFSM.ino
+++ RobotUltimateFSM.ino
@@ -3,7 +3,7 @@
 #include <TimerFive.h>
 
 #define BELT_PITCH 2.032 // distance between teeth
-#define NTEETH 90.0 // number of teeth 
+#define NTEETH 90.0 // number of teeth
 
 #define RPM_2_MMS BELT_PITCH*NTEETH/60.0 // conversion of the speed from rpm to mm/s
 
@@ -35,13 +35,14 @@ float d01 = 0;
 float d02 = 0;
 float Te = (Ts/1000)/1000.0f;
 
-volatile float angle = 0;  //actual heading in deg
-volatile float speed1 = 0; //actual speed in mm/s
+volatile float angle = 0; // actual heading in deg
+volatile float angle_point = 0;
+volatile float speed1 = 0; // actual speed in mm/s
 volatile float speed2 = 0;
-volatile float position1 = 0; //actual poition in mm
+volatile float position1 = 0; // actual position in mm
 volatile float position2 = 0;
-volatile float ref1 = 0; //actual references from serial
-volatile float ref2 = 0;
+volatile float ref1 = 0; // actual reference speed from serial
+volatile float refAngle = 0; // actual reference angular speed from serial
 float u1 = 0; // control signals in V
 float u2 = 0;
 volatile long compTime = 0; // actual computation time of the critical loop
@@ -52,217 +53,222 @@ MeEncoderOnBoard Encoder_1(SLOT1); // motor with encoder object instanciation
 MeEncoderOnBoard Encoder_2(SLOT2);
 
 /*
- * routine that is called every 5ms by the ISR routine
- */
 
+routine that is called every 5ms by the ISR routine
+*/
 void Update5ms()
 {
-  UpdateSensors();
-  UpdateControl();
-  UpdateActuators();
+    UpdateSensors();
+    UpdateControl();
+    UpdateActuators();
 }
 
 float calcU(float u) {
-  if (u > 9.0f)
-    return 9.0f;
-  else if (u < -9.0f)
-    return -9.0f;
-  return u;
+    if (u > 9.0f)
+        return 9.0f;
+    else if (u < -9.0f)
+        return -9.0f;
+    return u;
 }
 
 void UpdateSensors(){
-  gyro.update(); // update the gyroscope state
-  angle = gyro.getAngleZ(); // get the estimated heading in deg
-  
-  Encoder_1.loop(); // update the encoders state
-  Encoder_2.loop();
-  
-  speed1 = Encoder_1.getCurrentSpeed()*RPM_2_MMS; // compute the speed in mm/s
-  speed2 = Encoder_2.getCurrentSpeed()*RPM_2_MMS;  
+    float angle_anc = angle ;
+    gyro.update(); // update the gyroscope state
+    angle = gyro.getAngleZ(); // get the estimated heading in deg
+    angle_point = (angle - angle_anc)/Te ;
+    Encoder_1.loop(); // update the encoders state
+    Encoder_2.loop();
 
-  position1 = Encoder_1.getCurPos()*BELT_PITCH*NTEETH/360.0f; // compute the position in mm
-  position2 = Encoder_2.getCurPos()*BELT_PITCH*NTEETH/360.0f;  
+    speed1 = Encoder_1.getCurrentSpeed()*RPM_2_MMS; // compute the speed in mm/s
+    speed2 = Encoder_2.getCurrentSpeed()*RPM_2_MMS;
+
+    position1 = Encoder_1.getCurPos()*BELT_PITCH*NTEETH/360.0f; // compute the position in mm
+    position2 = Encoder_2.getCurPos()*BELT_PITCH*NTEETH/360.0f;
 }
 
 void UpdateControl()
 {
-  // Update FSM
-  e1 = position1 - d01;
-  float d01temp = d01 + Te * (v01+ e1* l1);
-  float v01temp = v01 + Te * (a * v01 + b * u1 + b * p01 + l2 * e1 );
-  float p01temp = p01 + Te * l3 * e1;
+    // Update FSM
+    e1 = position1 - d01;
+    float d01temp = d01 + Te * (v01 + e1 * l1);
+    float v01temp = v01 + Te * (a * v01 + b * u1 + b * p01 + l2 * e1 );
+    float p01temp = p01 + Te * l3 * e1;
 
-  e2 = position2 - d02;
+    e2 = position2 - d02;
+    float d02temp = d02 + Te * (v02 + e2 * l1);
+    float v02temp = v02 + Te * (a * v02 + b * u2 + b * p02 + l2 * e2 );
+    float p02temp = p02 + Te * l3 * e2;
 
-  float d02temp = d02 + Te * (v02+ e2* l1);
-  float v02temp = v02 + Te * (a * v02 + b * u2 + b * p02 + l2 * e2 );
-  float p02temp = p02 + Te * l3 * e2;
+    d01 = d01temp;
+    v01 = v01temp;
+    p01 = p01temp;
 
-  d01 = d01temp;
-  v01 = v01temp;
-  p01 = p01temp;
+    d02 = d02temp;
+    v02 = v02temp;
+    p02 = p02temp;
 
-  d02 = d02temp;
-  v02 = v02temp;
-  p02 = p02temp;
+    float refSpeedDifference = (refAngle * PI)/180 * 124.5f; // Compute X based on refAngle
+    ref1 = - refSpeedDifference;
+    float ref2 = refSpeedDifference;
 
-  float xi01temp = xi01   + Te * (- ref1 - v01 + Kb*(calcU(u1)-u1));
-  float u10 = -kI * xi01 - kx * v01;
+    float xi01temp = xi01 + Te * (- ref1 - v01 + Kb*(calcU(u1)-u1));
+    float u10 = -kI * xi01 - kx * v01;
 
-  float xi02temp = xi02   + Te * (ref2 - v02 + Kb*(calcU(u2)-u2));
-  float u20 = -kI * xi02 - kx * v02;
+    float xi02temp = xi02 + Te * (ref2 - v02 + Kb*(calcU(u2)-u2));
+    float u20 = -kI * xi02 - kx * v02;
 
-  xi01 = xi01temp;
-  xi02 = xi02temp;
-  u1 = u10;
-  u2 = xi02;
-
+    xi01 = xi01temp;
+    xi02 = xi02temp;
+    u1 = u10;
+    u2 = u20;
 }
 
 void UpdateActuators(){
-  setMotorsVoltage(u1,u2); // set the voltages
+    setMotorsVoltage(u1, u2); // set the voltages
 }
+
 /*
-* routine that is called every 5ms by the timer 5
+
+routine that is called every 5ms by the timer 5
 */
 void Timer5ISR(){
-  static char executing = 0; // set to 1 when the update function is running
-  if(executing) {
-    overrun = 1;
-    return;
-  }
-  else executing = 1; // if already running => overrunned
+    static char executing = 0; // set to 1 when the update function is running
+    if(executing) {
+        overrun = 1;
+        return;
+    }
+    else executing = 1; // if already running => overrun
 
-  interrupts(); // enable the interrupts during the execution of this loop
-  long startTime = micros();
-  
-  Update5ms();
-  
-  compTime = micros()-startTime;
-  executing = 0;
+    interrupts(); // enable the interrupts during the execution of this loop
+    long startTime = micros();
+
+    Update5ms();
+
+    compTime = micros()-startTime;
+    executing = 0;
 }
 
 /*
- * interruption routine for encoder pulse counting (quadrature)
- */
 
+interruption routine for encoder pulse counting (quadrature)
+*/
 void isr_process_encoder1(void)
 {
-  if(digitalRead(Encoder_1.getPortB()) == 0)
-  {
-    Encoder_1.pulsePosMinus();
-  }
-  else
-  {
-    Encoder_1.pulsePosPlus();;
-  }
+    if(digitalRead(Encoder_1.getPortB()) == 0)
+    {
+        Encoder_1.pulsePosMinus();
+    }
+    else
+    {
+        Encoder_1.pulsePosPlus();;
+    }
 }
 
 void isr_process_encoder2(void)
 {
-  if(digitalRead(Encoder_2.getPortB()) == 0)
-  {
-    Encoder_2.pulsePosMinus();
-  }
-  else
-  {
-    Encoder_2.pulsePosPlus();
-  }
+    if(digitalRead(Encoder_2.getPortB()) == 0)
+    {
+        Encoder_2.pulsePosMinus();
+    }
+    else
+    {
+        Encoder_2.pulsePosPlus();
+    }
 }
 
 /*
- * Setup the motors with the reduction ratio and number of pulses per turn
- */
 
+Setup the motors with the reduction ratio and number of pulses per turn
+*/
 void setupMotors(){
-  Encoder_1.setPulse(8);
-  Encoder_1.setRatio(46);
-  Encoder_2.setPulse(8);
-  Encoder_2.setRatio(46);
-  
-  attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
-  attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
-  //Set PWM 8KHz
-  TCCR1A = _BV(WGM10);
-  TCCR1B = _BV(CS11) | _BV(WGM12);
+    Encoder_1.setPulse(8);
+    Encoder_1.setRatio(46);
+    Encoder_2.setPulse(8);
+    Encoder_2.setRatio(46);
 
-  TCCR2A = _BV(WGM21) | _BV(WGM20);
-  TCCR2B = _BV(CS21);
+    attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
+    attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
+    // Set PWM 8KHz
+    TCCR1A = _BV(WGM10);
+    TCCR1B = _BV(CS11) | _BV(WGM12);
+
+    TCCR2A = _BV(WGM21) | _BV(WGM20);
+    TCCR2B = _BV(CS21);
 }
-
 
 /*
- * Sets the voltage to the motor. Limited by MAX_VOLTAGE
- */
+
+Sets the voltage to the motor. Limited by MAX_VOLTAGE
+*/
 void setMotorsVoltage(float voltage1,float voltage2){
-  Encoder_1.setMotorPwm(constrain(voltage1,-MAX_VOLTAGE,MAX_VOLTAGE)*VOLTS_2_PWM);
-  Encoder_2.setMotorPwm(constrain(voltage2,-MAX_VOLTAGE,MAX_VOLTAGE)*VOLTS_2_PWM);
+    Encoder_1.setMotorPwm(constrain(voltage1,-MAX_VOLTAGE,MAX_VOLTAGE)*VOLTS_2_PWM);
+    Encoder_2.setMotorPwm(constrain(voltage2,-MAX_VOLTAGE,MAX_VOLTAGE)*VOLTS_2_PWM);
 }
-
-
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.setTimeout(1);
-  gyro.begin();
-  Wire.setClock(400000);
-  setupMotors();
-  Timer5.initialize(Ts);
-  Timer5.attachInterrupt(Timer5ISR);  
+    Serial.begin(115200);
+    Serial.setTimeout(1);
+    gyro.begin();
+    Wire.setClock(400000);
+    setupMotors();
+    Timer5.initialize(Ts);
+    Timer5.attachInterrupt(Timer5ISR);
 }
-
 
 void loop()
 {
-  static float lastRef1 = 0;
-  static float lastRef2 = 0;
-  
-  noInterrupts();
-  ref1 = lastRef1;
-  ref2 = lastRef2;
-  float angleCopy = angle;
-  float speed1Copy = speed1;
-  float speed2Copy = speed2;
-  float position1Copy = position1;
-  float position2Copy = position2;
-  long compTimeCopy = compTime;
-  interrupts();
-  
-  //Serial.read();
-  Serial.print(" compTime: ");
-  Serial.print(compTimeCopy);
+    static float lastRef1 = 0;
+    static float lastRefAngle = 0;
 
-  Serial.print(" overrun: ");
-  Serial.print(overrun);
-  
-  Serial.print(" speed1: ");
-  Serial.print(speed1Copy,2);
-  
-  Serial.print(" speed2: ");
-  Serial.print(speed2Copy,2);
-  
-  Serial.print(" position1: ");
-  Serial.print(position1Copy,2);
-  
-  Serial.print(" position2: ");
-  Serial.print(position2Copy,2);
+    noInterrupts();
+    ref1 = lastRef1;
+    refAngle = lastRefAngle;
+    float angleCopy = angle;
+    float angle_pointCopy = angle_point;
+        float speed1Copy = speed1;
+    float speed2Copy = speed2;
+    float position1Copy = position1;
+    float position2Copy = position2;
+    long compTimeCopy = compTime;
+    interrupts();
 
-  Serial.print(" angle: ");
-  Serial.print(angleCopy,2);
+    // Serial.read();
+    // Serial.print(" compTime: ");
+    // Serial.print(compTimeCopy);
 
-  Serial.print(" ref1: ");
-  Serial.print(ref1,2);
+    // Serial.print(" overrun: ");
+    // Serial.print(overrun);
 
-  Serial.print(" ref2: ");
-  Serial.print(ref2,2);
-  
-  
-  Serial.println();
+    // Serial.print(" speed1: ");
+    // Serial.print(speed1Copy,2);
 
-  if(Serial.available()){
-    lastRef1 = Serial.parseFloat();
-    lastRef2 = Serial.parseFloat();
-  }
-  delay(10);
+    // Serial.print(" speed2: ");
+    // Serial.print(speed2Copy,2);
+
+    // Serial.print(" position1: ");
+    // Serial.print(position1Copy,2);
+
+    // Serial.print(" position2: ");
+    // Serial.print(position2Copy,2);
+
+    // Serial.print(" angle: ");
+    // Serial.print(angleCopy,2);
+
+    //Serial.print(" angle_point: ");
+    Serial.print(angle_pointCopy,2);
+
+    // Serial.print(" ref1: ");
+    // Serial.print(ref1,2);
+
+    // Serial.print(" refAngle: ");
+    // Serial.print(refAngle,2);
+
+    Serial.println();
+
+    if(Serial.available()){
+        lastRef1 = Serial.parseFloat();
+        lastRefAngle = Serial.parseFloat();
+    }
+    delay(10);
 }
